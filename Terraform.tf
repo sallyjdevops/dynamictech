2b94f96b236aac132f0a319d89efbef1592cd96d
unknown <mbengs@yahoo.com>
2024-04-14 18:08:26 +0200

    Adding txt file & updated Terraform code

--- Terraform.tf
+++ Terraform.tf
@@ -7,3 +7,13 @@ variable "ami_id" {
     type = string
     description = "Enter AMI ID"
 }
+
+
+module "create_ec2_instance" {
+    source = "./modules/EC2"
+    resource_env = var.resource_env
+    ami_id = var.ami_id 
+    instance_type = var.instance_type
+    Owner_number = "12345"
+    owner_name = local.owner_name
+}
